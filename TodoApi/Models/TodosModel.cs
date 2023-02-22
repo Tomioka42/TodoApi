7b40844331b02396ac957ca3264d511c06fc2411
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-02-22 12:06:15 +0100

    Made The Database
    Database created

---
+++ TodoApi/Models/TodosModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class TodosModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
