67981b1996b0b319dedd7ec64e23995433bd6199
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-02-22 12:56:30 +0100

    Lagt till alla functions

--- TodoApi/Controllers/TodosController.cs
+++ TodoApi/Controllers/TodosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Data;
+using TodoApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +10,84 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodosController : ControllerBase
     {
+        private readonly AppDbContext context;
+        public TodosController(AppDbContext context)
+        {
+            this.context = context;
+        }
         // GET: api/<TodosController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<TodosModel>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(context.Todos.ToList());
         }
 
         // GET api/<TodosController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<TodosModel?> Get(int id)
         {
-            return "value";
+            return Ok(context.Todos.FirstOrDefault(t => t.Id == id));
         }
 
         // POST api/<TodosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] TodosModel todo)
         {
+            List<TodosModel> todos = context.Todos.ToList();
+
+            TodosModel? foundTodo = todos.FirstOrDefault(t => t.Title == todo.Title);
+
+            if (foundTodo == null)
+            {
+                context.Todos.Add(todo);
+                context.SaveChanges();
+
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Todo already exists!");
+            }
         }
 
         // PUT api/<TodosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] TodosModel todo)
         {
+            List<TodosModel> todos = context.Todos.ToList();
+
+            var newTodo = todos.FirstOrDefault(t => t.Id == todo.Id);
+            if (newTodo == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                newTodo.Title = todo.Title;
+                newTodo.Description = todo.Description;
+
+                context.Update(newTodo);
+                context.SaveChanges();
+
+                return NoContent();
+            }
         }
 
         // DELETE api/<TodosController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var todoToRemove = context.Todos.FirstOrDefault(t => t.Id == id);
+            if (todoToRemove == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                context.Todos.Remove(todoToRemove);
+
+                return NoContent();
+            }
         }
     }
 }
